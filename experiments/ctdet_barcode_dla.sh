0eb0152cb4e3a75efc56faa3117e93ac65970716
Nam Doan Ngoc <nam.dng2511@gmail.com>
2019-12-17 09:42:32 +0700

    update test command to test on barcode val dataset

--- experiments/ctdet_barcode_dla.sh
+++ experiments/ctdet_barcode_dla.sh
@@ -1,5 +1,8 @@
 # train
 python main.py ctdet --exp_id dla --batch_size 16 --lr 1.25e-4  --gpus 0 --dataset barcode
 
+# test
+python test.py ctdet --exp_id dla --dataset barcode --load_model ../exp/ctdet/dla/model_best.pth
+
 # demo
 python demo.py ctdet --dataset barcode --exp_id dla --demo '/hdd/namdng/barcode/python/BarcodeDatasets/exp_aligned' --load_model ../exp/ctdet/dla/model_best.pth --gpus 0
